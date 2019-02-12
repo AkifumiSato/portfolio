e5c34fc35c3f96cd74a313641739f04104681281
佐藤昭文 <>
2019-02-12 22:36:47 +0900

    APIのIFを変更、mapで自動生成に

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import get from 'lodash/get'
+import head from 'lodash/head'
 import Helmet from 'react-helmet'
 import Link from 'gatsby-link'
 import styles from './about.module.css'
@@ -8,38 +9,29 @@ import MainTitle from '../components/main-title'
 class RootIndex extends React.Component {
   render() {
     const siteTitle = `About - ${get(this, 'props.data.site.siteMetadata.title')}`
-    const posts = get(this, 'props.data.allContentfulPerson.edges[0].node')
-    const iAmText = get(posts, 'iAm.content').map((obj) => {
-      return obj.content[0].value
-    })
-    const skillText = get(posts, 'skill.content').map((obj) => {
-      return obj.content[0].value
-    })
-    const carrerText = get(posts, 'carrer.content').map((obj) => {
-      return obj.content[0].value
-    })
+    const posts = get(this, 'props.data.allContentfulPerson.edges[0].node.about.content')
 
     return (
       <div>
         <Helmet title={ siteTitle } />
         <MainTitle title="ABOUT" />
-        <div>
-          <div className={ styles.inner }>
-            <h2 className={ styles.sub_title }>I am ...</h2>
-            <p className={ styles.text }>{ iAmText }</p>
-          </div>
-          <div className={ styles.inner }>
-            <h2 className={ styles.sub_title }>skill</h2>
-            <p className={ styles.text }>{ skillText }</p>
-          </div>
-          <div className={ styles.inner }>
-            <h2 className={ styles.sub_title }>career</h2>
-            <p className={ styles.text }>{ carrerText }</p>
-          </div>
-          <div className={ styles.inner }>
-            <h2 className={ styles.sub_title }>contact</h2>
-            <p className={ styles.text }><Link to={ '/contact/' } className={ styles.link }>&#x203A; go to contact</Link></p>
-          </div>
+        <div className={ styles.wrap }>
+          { posts.map((({content, nodeType}, index) => {
+            switch (nodeType) {
+              case 'heading-2':
+                return (
+                  <h2 key={ index } className={ styles.sub_title }>{ head(content).value }</h2>
+                )
+              case 'paragraph':
+                return (
+                  <p key={ index } className={ styles.text }>{ head(content).value }</p>
+                )
+              default:
+                break
+            }
+          })) }
+          <h2 className={ styles.sub_title }>contact</h2>
+          <p className={ styles.text }><Link to={ '/contact/' } className={ styles.link }>&#x203A; go to contact</Link></p>
         </div>
       </div>
     )
@@ -53,23 +45,11 @@ export const pageQuery = graphql`
     allContentfulPerson {
       edges {
         node {
-          iAm {
+          about {
             content {
+              nodeType
               content {
-                value
-              }
-            }
-          }
-          skill {
-            content {
-              content {
-                value
-              }
-            }
-          }
-          carrer {
-            content {
-              content {
+                nodeType
                 value
               }
             }
