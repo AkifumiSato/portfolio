39136e3c4f7f1793452bea9bb04bc74e765902f1
佐藤昭文 <>
2019-03-13 07:06:36 +0900

    一覧での画像表示を最適化

--- src/components/article-preview.js
+++ src/components/article-preview.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import styles from './article-preview.module.css'
 import Link from 'gatsby-link'
+import Img from 'gatsby-image'
 
-export default ({ article }) => {
+export default ({ article, heroImage }) => {
   return (
-    <Link to={`/blog/${article.slug}`} className={styles.link}>
-      <p className={styles.day}>{article.publishDate}</p>
-      <img src={article.heroImage.sizes.srcWebp} className={styles.image} alt="" />
-      <div className={styles.body}>
-        <h3 className={styles.title}>{article.title}</h3>
-        <p className={styles.paragraph}>{article.description.description}</p>
+    <Link to={ `/blog/${article.slug}` } className={ styles.link }>
+      <p className={ styles.day }>{ article.publishDate }</p>
+      <Img sizes={ heroImage.sizes } className={ styles.image } alt="" />
+      <div className={ styles.body }>
+        <h3 className={ styles.title }>{ article.title }</h3>
+        <p className={ styles.paragraph }>{ article.description.description }</p>
       </div>
     </Link>
   )
