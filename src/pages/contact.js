33a812596aec00057d625de86fa5051ee5070bc5
佐藤昭文 <>
2019-03-27 22:33:51 +0900

    form 初稿デザイン

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,8 +2,34 @@ import React from 'react'
 import get from 'lodash/get'
 import Helmet from 'react-helmet'
 import MainTitle from '../components/main-title'
+import styles from './contact.module.css'
+import fonts from '../components/utils/fonts.module.css'
 
 class RootIndex extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    const form = e.target
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': form.getAttribute('name'),
+        ...this.state,
+      }),
+    })
+      .then(() => navigateTo(form.getAttribute('action')))
+      .catch(error => alert(error))
+  }
+
   render() {
     const siteTitle = `CONTACT - ${get(this, 'props.data.site.siteMetadata.title')}`
 
@@ -11,14 +37,39 @@ class RootIndex extends React.Component {
       <div>
         <Helmet title={ siteTitle } />
         <MainTitle title="CONTACT" />
-        <div>
-          <p style={
-            {
-              color: '#aaa',
-              fontSize: '30px',
-              textAlign: 'center',
-            }
-          }>作成中...</p>
+        <div className={ styles.wrapper }>
+          <form
+            name="contact"
+            method="post"
+            // action="/thanks/"
+            // data-netlify="true"
+            // data-netlify-honeypot="bot-field"
+            // onSubmit={this.handleSubmit}
+          >
+            <div className={ styles.itemWrapper }>
+              <h2 className={ styles.itemTitle }>Name</h2>
+              <div className={ styles.inputText }>
+                <input type="text" placeholder='田中 太郎' />
+              </div>
+            </div>
+            <div className={ styles.itemWrapper }>
+              <h2 className={ styles.itemTitle }>Mail</h2>
+              <div className={ styles.inputText }>
+                <input type="mail" placeholder='xxxxx@xxx.com' />
+              </div>
+            </div>
+            <div className={ styles.itemWrapper }>
+              <h2 className={ styles.itemTitle }>Message</h2>
+              <div className={ styles.textarea }>
+                <textarea className={ styles.textarea } cols="30" rows="5" maxLength="1000" />
+              </div>
+            </div>
+            <div className={ styles.buttonWrapper }>
+              <button type="submit" className={ fonts.raleway }>Send</button>
+            </div>
+            { /* The `form-name` hidden field is required to support form submissions without JavaScript */ }
+            <input type="hidden" name="form-name" value="contact" />
+          </form>
         </div>
       </div>
     )
