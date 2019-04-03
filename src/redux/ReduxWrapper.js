cc3f52a8f3f642f52ea4f2683028cd151487e9cb
佐藤昭文 <>
2019-04-04 00:28:59 +0900

    production build error patch

--- src/redux/ReduxWrapper.js
+++ src/redux/ReduxWrapper.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import { createStore as reduxCreateStore } from 'redux'
+import { createStore as reduxCreateStore, compose } from 'redux'
 import rootReducer from './rootReducer'
+
+const windowGlobal = typeof window !== 'undefined' && window
+
+const devtools =
+  process.env.NODE_ENV === 'development' && windowGlobal.devToolsExtension
+    ? window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__()
+    : f => f
 
 const createStore = () => reduxCreateStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  compose(
+    devtools,
+  ),
 )
 export default ({ element }) => (
   <Provider store={ createStore() }>{ element }</Provider>
