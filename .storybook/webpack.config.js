24b7643325a49fcd808b4f6cd557a6d9f5f9fe5c
佐藤昭文 <>
2019-05-18 20:54:15 +0900

    addon add, storyの修正

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -11,6 +11,10 @@ module.exports = ({ config }) => {
       {
         loader: require.resolve('react-docgen-typescript-loader'),
       },
+      {
+        loader: require.resolve('@storybook/addon-storysource/loader'),
+        options: { parser: 'typescript' }
+      }
     ],
   });
   config.resolve.extensions.push('.ts', '.tsx');
--- src/components/atoms/FormError/index.stories.tsx
+++ src/components/atoms/FormError/index.stories.tsx
@@ -5,25 +5,29 @@ import { GlobalStyles } from '../../../styles/global'
 import styled from 'styled-components'
 import FormError from './index'
 
-const MyInput = styled.input`
+const MyText = styled.p`
   border: 1px solid #ccc;
   border-radius: 3px;
   -webkit-appearance: none;
   padding: 10px;
   width: 30%;
 `
+const StoryWrapper = (storyFn: () => React.FC) => (
+  <div style={ {
+    margin: '30px',
+    position: 'relative'
+  } }>
+    <GlobalStyles />
+    <MyText>sample</MyText>
+    { storyFn() }
+  </div>
+)
 
 storiesOf(FormError.name, module)
+  .addDecorator(StoryWrapper)
   .add('FormError',
     withInfo(`
     formパーツでエラーを表現するのに使用します。
     `)(() =>
-      <div style={{
-        margin: '30px',
-        position: 'relative'
-      }}>
-        <GlobalStyles />
-        <MyInput value="sample" />
-        <FormError text="名前は必須です。" />
-      </div>
+      <FormError text="名前は必須です。" />
     ))
