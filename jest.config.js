563179ca6ed9bb806debaa3fc125129062cc91a4
佐藤昭文 <akfm.sato@gmail.com>
2021-02-15 23:09:04 +0900

    jest conf update

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,8 @@
 module.exports = {
   transform: {
-    "^.+\\.[jt]sx?$": "<rootDir>/jest-preprocess.js",
+    '^.+\\.[jt]sx?$': 'esbuild-jest',
   },
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.([tj]sx?)$",
-  moduleNameMapper: {
-    ".+\\.(css|styl|less|sass|scss)$": "identity-obj-proxy",
-    ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "<rootDir>/__mocks__/file-mock.js",
-  },
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  testPathIgnorePatterns: ["node_modules", ".cache", "dist", "public"],
-  transformIgnorePatterns: ["node_modules/(?!(gatsby)/)"],
-  globals: {
-    __PATH_PREFIX__: "",
-  },
-  testURL: "http://localhost",
-  setupFiles: ["<rootDir>/jest-loadershim.js"],
-  setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.([tj]sx?)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
+  testPathIgnorePatterns: ['node_modules', '.cache', 'dist', 'public'],
 }
